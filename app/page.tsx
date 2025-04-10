8fce382b8a4af2c0dfea7cd6d2f7bf76131f29e5
agerskov <jokkexx7@gmail.com>
2025-04-10 19:01:13 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,78 @@
-import Image from "next/image";
+import { User, Phone, MapPin } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="flex sticky top-0 left-0 z-20 justify-center items-center py-5 w-screen bg-black">
+        <div className="w-[80%] flex gap-12 items-center">
+          <h1 className="text-3xl text-blue-500">Karlskov Auto</h1>
+          <ul className="hidden gap-5 md:flex">
+            <li>åbningstider</li>
+            <li>kontakt</li>
+          </ul>
         </div>
+      </header>
+      <main>
+        <section className="flex relative flex-col items-center pt-7 w-screen h-screen">
+          <div className="absolute inset-0 z-0 bg-center bg-cover brightness-30 bg-[url(/images/hero.jpg)]"></div>
+          <div className="flex relative z-10 flex-col gap-7 text-center">
+            <h2 className="text-5xl text-blue-500">
+              Dit nye lokale autoværksted
+            </h2>
+            <p className="mx-auto w-3/4 text-xl">
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
+              dolorem ipsum quia dolor sit amet
+            </p>
+          </div>
+        </section>
+        <section className="flex flex-col items-center pt-12">
+          <div className="w-[80%] flex flex-col gap-20">
+            <h2 className="text-5xl text-center md:text-start">Åbningstider</h2>
+            <div className="flex flex-col gap-4 justify-between md:flex-row">
+              <div className="flex flex-col gap-2 items-center">
+                <h3 className="text-5xl text-blue-500">16:00-21:00</h3>
+                <p className="text-2xl">mandag-torsdag</p>
+              </div>
+              <div className="flex flex-col gap-2 items-center">
+                <h3 className="text-5xl text-blue-500">16:00-19:00</h3>
+                <p className="text-2xl">fredag</p>
+              </div>
+              <div className="flex flex-col gap-2 items-center">
+                <h3 className="text-5xl text-blue-500">Efter aftale</h3>
+                <p className="text-2xl">lørdag-søndag</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="flex flex-col items-center py-28">
+          <div className="w-[80%] flex flex-col  gap-7">
+            <h2 className="text-5xl">Kontakt</h2>
+            <div className="flex flex-col gap-7 md:flex-row">
+              <div className="w-[50%] aspect-square">
+                <img
+                  src="https://placehold.co/600x400"
+                  className="object-cover w-full h-full"
+                />
+              </div>
+              <div className="flex flex-col gap-3 text-xl md:gap-7 md:text-3xl">
+                <div className="flex gap-2 items-center">
+                  <MapPin className="text-blue-500" size={32} />
+                  <p>Teknikvej 4, 4532 Gislinge</p>
+                </div>
+
+                <div className="flex gap-2 items-center">
+                  <Phone className="text-blue-500" size={32} />
+                  <p>24491915</p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <User className="text-blue-500" size={32} />
+                  <p>Henrik Karlskov</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
